9219c0ef36cd2bba92af4b4c01dae5f3ff63c11b
Kumar Thangaraj <23ds1000074@ds.study.iitm.ac.in>
2026-07-05 22:01:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -10,9 +10,7 @@ origins = [
 ]
 
 app.add_middleware(
-    CORSMiddleware,
     allow_origins=origins,          # Do not use ["*"] for real apps with login/cookies
-    allow_credentials=True,         # Allow cookies / Authorization headers
     allow_methods=["GET", "POST", "PATCH", "DELETE"],
     allow_headers=["Authorization", "Content-Type"],
 )
